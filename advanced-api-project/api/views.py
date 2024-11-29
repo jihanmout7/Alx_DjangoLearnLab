97422b4c07a786cbcde6d4ee9ecc454de7566913
Jihane <moutalebjihane90@gmail.com>
2024-11-29 21:55:22 +0000

    first commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,8 +2,9 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import filters
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+
 
 # List of Books (GET request)
 class BookListView(generics.ListAPIView):
