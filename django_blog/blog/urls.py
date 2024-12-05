21fa53c527f95abd3a36f75730dfb50f00fec545
Jihane <moutalebjihane90@gmail.com>
2024-12-05 20:24:45 +0000

    first commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,9 @@
-# blog/urls.py
 from django.urls import path
-from django.contrib.auth.views import LoginView  # Import the built-in LoginView
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),  # Use Django's LoginView
-    path('logout/', views.logout_view, name='logout'),  # Assuming you have a custom logout view
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 ]
