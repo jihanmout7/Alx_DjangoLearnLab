d7ef9d7d78b0982fdd134967a6ba87c45d5fd131
Jihane <moutalebjihane90@gmail.com>
2024-12-09 23:01:00 +0000

    first commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,33 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    # Define password as a write-only field
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-    token =serializers.CharField(Token.objects.create(user=User)) 
 
     class Meta:
         model = get_user_model()
-        fields ='__all__'
+        fields = ['username', 'email', 'phone_number', 'bio', 'password', 'password2']
         extra_kwargs = {
             'email': {'required': True, 'validators': [UniqueValidator(queryset=get_user_model().objects.all())]},
         }
 
     def validate(self, data):
-        # Ensure passwords match
+        # Ensure that password2 matches password
         if data['password'] != data['password2']:
             raise ValidationError({"password2": "The two password fields didn't match."})
         return data
 
     def create(self, validated_data):
-        # Remove password2 as it's not needed after validation
+        # Remove password2 since it's only for confirmation
         validated_data.pop('password2')
 
         # Create the user
@@ -39,9 +35,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', '')
         )
 
-        # Create token after user creation
-        
+        # Create a token for the user after they are successfully created
+        token = Token.objects.create(user=user)
 
         return {
             'user': user,
+            'token': token.key  # Return the token key (JWT or Auth token)
         }
